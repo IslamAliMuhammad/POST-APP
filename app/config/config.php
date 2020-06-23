431d0d35dc58216235a79cbb37790a949358353e
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2020-06-23 22:09:53 +0200

    feat: Navigate through about and home pages through navigation bar

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,14 @@
 <?php
     // DATABASE
     define('DB_HOST', 'localhost');
-    define('DB_USER', ' '); // Input your database user name 
-    define('DB_PASS', ' '); // Input your database password
-    define('DB_NAME', ' '); // Input your database name
+    define('DB_USER', 'root'); // Input your database user name 
+    define('DB_PASS', '12345'); // Input your database password
+    define('DB_NAME', 'shareposts'); // Input your database name
 
     // APP
     define('APPROOT', dirname(dirname(__FILE__)));
-    define('URLROOT', ' '); //Input your url
-    define('SITENAME', ' '); // Input your sitename
+    define('URLROOT', 'http://localhost:8081/shareposts'); //Input your url 
+    define('SITENAME', 'SharePosts'); // Input your sitename
+    define('APPVERSION', '1.0.0'); // Input your app version
 
 ?>
