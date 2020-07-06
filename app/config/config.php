e13be2def89ffae883e68ab99e272cb1d9304aad
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2020-07-06 05:16:58 +0200

    chore: Release some configurations

--- app/config/config.php
+++ app/config/config.php
@@ -2,12 +2,12 @@
     // DATABASE
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root'); // Input your database user name 
-    define('DB_PASS', '12345'); // Input your database password
+    define('DB_PASS', ''); // Input your database password
     define('DB_NAME', 'post'); // Input your database name
 
     // APP
     define('APPROOT', dirname(dirname(__FILE__)));
-    define('URLROOT', 'http://localhost:8081/post'); //Input your url 
+    define('URLROOT', 'http://localhost/post'); //Input your url 
     define('SITENAME', 'POST'); // Input your sitename
     define('APPVERSION', '1.0.0'); // Input your app version
 
